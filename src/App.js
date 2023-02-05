e572de98fdc8eace2df4880bd64d9af94b954636
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-02-05 14:08:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component{
 
       this.quebraBiscoito = this.quebraBiscoito.bind(this);
 
-      this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais que mil regras e leis ', 'Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível', 'Acredite em milagres, mas não dependa deles', 'A maior barreora para o sucesso é o medo do fracasso.'];
+      this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais que mil regras e leis ', 'Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível', 'Acredite em milagres, mas não dependa deles', 'A maior barreira para o sucesso é o medo do fracasso.'];
 
     }
 
