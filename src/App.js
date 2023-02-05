20b979ae2d030e2bba779d64da061486e877e12b
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-02-05 13:18:47 -0300

    BiscoitoDaSorte
    aplicação biscoito da sorte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component, Fragment }from "react";
+import './index.css'
+import Botao from "./components/Botao";
+import './estilo.css'
+
+class App extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = { 
+        textoFrase : ''
+
+
+      };
+
+      this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+      this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais que mil regras e leis ', 'Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível', 'Acredite em milagres, mas não dependa deles', 'A maior barreora para o sucesso é o medo do fracasso.'];
+
+    }
+
+    quebraBiscoito(){
+      let state = this.state;
+      let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+      state.textoFrase ='" ' + this.frases [numeroAleatorio] + ' "';
+      this.setState(state);
+
+    }
+
+  render(){
+    return(
+      <div className="container">
+          <img src={require('./assets/biscoito.png')} className="img"/>
+          <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+          <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      
+      </div>
+
+
+    );
+    
+
+  }
+  
 }
+
 
 export default App;
